49114d28e1fcec028be0eecdb79038999df6a54c
j0mp1x <b123zx@mail.ru>
2026-09-10 22:06:00 +0300

    добавил useGameState, разгрузил App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
 import { useEffect, useState, useRef } from 'react'
-import {
-    battleUpdate,
-    createFighter,
-    fighters,
-    updateFighter,
-} from './data/fighters'
-import {
-    Preset,
-    defaultPresets,
-    createPreset,
-    updatePreset,
-} from './data/preset'
 import ConfirmModal from './components/confirmModal'
 import FormModal from './components/formModal'
 import getDataFromForm from './UTILITES/getData'
 import { currentId, setCurrentId } from './UTILITES/idCreator'
+import useGameState from './hooks/useGameState'
 
 function App() {
-    const [gameState, setGameState] = useState({
-        fighters: fighters.sort((a, b) => b.initiative - a.initiative),
-        presets: [...defaultPresets],
-        round: 1,
-        currentFighter: 0,
-    })
-
-    const [prevGameState, setPrevGameState] = useState([{}])
-
-    const [actionError, setActionError] = useState(false)
-
     const [confirmModal, setConfirmModal] = useState({
         isOpen: false,
         onConfirm: null,
@@ -40,39 +18,41 @@ function App() {
         value: null,
     })
 
+    const [actionError, setActionError] = useState(false)
+
+    const onUseActionClick = () => {
+        const success = onUseAction()
+
+        if (!success) {
+            setActionError(true)
+
+            setTimeout(() => {
+                setActionError(false)
+            }, 1000)
+        }
+    }
+
+    const {
+        gameState,
+        onEndTurn,
+        backUp,
+        loadFromLocalStorage,
+        saveToLocalStorage,
+        addFighter,
+        editFighter,
+        deleteAllFighters,
+        deleteFighter,
+        addPreset,
+        editPreset,
+        deletePreset,
+        onEndCombat,
+        onReactionUse,
+        onUseAction,
+        battleUpdateFighter,
+        onAttack,
+    } = useGameState(setConfirmModal)
+
     const isFirstRender = useRef(true)
-
-    const saveToLocalStorage = () => {
-        localStorage.setItem('gameState', JSON.stringify(gameState))
-        localStorage.setItem('currentId', JSON.stringify(currentId))
-    }
-
-    const loadFromLocalStorage = () => {
-        let storedGameState = localStorage.getItem('gameState')
-        if (storedGameState) {
-            storedGameState = JSON.parse(storedGameState)
-            try {
-                setGameState((prev) => {
-                    return {
-                        ...storedGameState,
-                        fighters: storedGameState.fighters.map((e) => {
-                            return updateFighter(e, e)
-                        }),
-                        presets: storedGameState.presets.map((e) => {
-                            return updatePreset(e, e)
-                        }),
-                    }
-                })
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        let storedCurrentId = localStorage.getItem('currentId')
-        if (storedCurrentId) {
-            storedCurrentId = Number(JSON.parse(storedCurrentId))
-            setCurrentId(storedCurrentId)
-        }
-    }
 
     useEffect(() => {
         loadFromLocalStorage()
@@ -86,240 +66,6 @@ function App() {
         }
         saveToLocalStorage()
     }, [gameState, currentId])
-
-    const onReactionUse = (f) => {
-        setPrevGameState((prev) => {
-            return [...prev, gameState]
-        })
-        setGameState((prev) => {
-            return {
-                ...prev,
-                fighters: prev.fighters.map((e) => {
-                    if (e.id === f.id) {
-                        const newReaction = e.reaction ? false : true
-                        return { ...e, reaction: newReaction }
-                    } else return e
-                }),
-            }
-        })
-    }
-
-    const onEndTurn = () => {
-        if (gameState.fighters.length !== 0) {
-            setPrevGameState((prev) => {
-                return [...prev, gameState]
-            })
-            setGameState((prev) => {
-                if (prev.currentFighter + 1 === prev.fighters.length) {
-                    return {
-                        ...prev,
-                        currentFighter: 0,
-                        round: prev.round + 1,
-                        fighters: prev.fighters.map((e) => {
-                            return {
-                                ...e,
-                                reaction: true,
-                                action: e.maxActions,
-                            }
-                        }),
-                    }
-                } else {
-                    return {
-                        ...prev,
-                        currentFighter: prev.currentFighter + 1,
-                    }
-                }
-            })
-        }
-    }
-
-    const onEndCombat = () => {
-        setGameState((prev) => {
-            return {
-                ...prev,
-                currentFighter: 0,
-                round: 1,
-                fighters: prev.fighters.map((e) => {
-                    return {
-                        ...e,
-                        reaction: true,
-                        action: e.maxActions,
-                    }
-                }),
-            }
-        })
-        setConfirmModal({
-            isOpen: true,
-            onConfirm: deleteAllFighters,
-            message: 'Удалить всех бойцов?',
-        })
-    }
-
-    const onUseAction = () => {
-        if (!actionError) {
-            if (gameState.fighters.length != 0) {
-                if (gameState.fighters[gameState.currentFighter].action > 0) {
-                    setPrevGameState((prev) => {
-                        return [...prev, gameState]
-                    })
-                    setGameState((prev) => {
-                        return {
-                            ...prev,
-                            fighters: prev.fighters.map((e) => {
-                                if (
-                                    e.id ===
-                                    prev.fighters[prev.currentFighter].id
-                                ) {
-                                    return {
-                                        ...e,
-                                        action:
-                                            prev.fighters[prev.currentFighter]
-                                                .action - 1,
-                                    }
-                                } else return e
-                            }),
-                        }
-                    })
-                } else {
-                    setActionError(true)
-
-                    setTimeout(() => {
-                        setActionError(false)
-                    }, 1000)
-                }
-            } else {
-                setActionError(true)
-
-                setTimeout(() => {
-                    setActionError(false)
-                }, 1000)
-            }
-        }
-    }
-
-    const onAttack = () => {
-        let currentFighter = gameState.fighters[gameState.currentFighter]
-
-        battleUpdateFighter(
-            gameState.fighters[gameState.currentFighter + 1],
-            currentFighter.attack()
-        )
-    }
-
-    const addFighter = (data) => {
-        setPrevGameState((prev) => {
-            return [...prev, gameState]
-        })
-        setGameState((prev) => {
-            return {
-                ...prev,
-                fighters: [...prev.fighters, createFighter(data)].sort(
-                    (a, b) => b.initiative - a.initiative
-                ),
-            }
-        })
-
-        if (data.saveAsPreset) {
-            addPreset(data)
-        }
-    }
-
-    const deleteFighter = (fighter) => {
-        setPrevGameState((prev) => {
-            return [...prev, gameState]
-        })
-        setGameState((prev) => {
-            return {
-                ...prev,
-                fighters: prev.fighters
-                    .filter((e) => e.id !== fighter.id)
-                    .sort((a, b) => b.initiative - a.initiative),
-            }
-        })
-    }
-
-    const deleteAllFighters = () => {
-        setPrevGameState((prev) => {
-            return [...prev, gameState]
-        })
-        setGameState((prev) => {
-            return {
-                ...prev,
-                fighters: [],
-            }
-        })
-    }
-
-    const editFighter = (data, fighter) => {
-        setPrevGameState((prev) => {
-            return [...prev, gameState]
-        })
-        setGameState((prev) => {
-            return {
-                ...prev,
-                fighters: prev.fighters.map((e) => {
-                    if (e.id === fighter.id) {
-                        return updateFighter(data, fighter)
-                    } else return e
-                }),
-            }
-        })
-    }
-
-    const battleUpdateFighter = (fighter, damage) => {
-        setPrevGameState((prev) => {
-            return [...prev, gameState]
-        })
-        setGameState((prev) => {
-            return {
-                ...prev,
-                fighters: prev.fighters.map((e) => {
-                    if (e.id === fighter.id) {
-                        return battleUpdate(fighter, damage)
-                    } else return e
-                }),
-            }
-        })
-    }
-
-    const addPreset = (data) => {
-        setGameState((prev) => {
-            return { ...prev, presets: [...prev.presets, createPreset(data)] }
-        })
-    }
-
-    const deletePreset = (preset) => {
-        setGameState((prev) => {
-            return {
-                ...prev,
-                presets: [...prev.presets].filter((e) => e.id !== preset.id),
-            }
-        })
-    }
-
-    const editPreset = (data, preset) => {
-        setGameState((prev) => {
-            return {
-                ...prev,
-                presets: prev.presets.map((e) => {
-                    if (e.id === preset.id) {
-                        return updatePreset(data, preset)
-                    } else return e
-                }),
-            }
-        })
-    }
-
-    const backUp = () => {
-        if (prevGameState.length > 1) {
-            setGameState((prev) => {
-                return { ...prevGameState[prevGameState.length - 1] }
-            })
-            setPrevGameState((prev) => {
-                return [...prev].slice(0, -1)
-            })
-        }
-    }
 
     return (
         <div>
@@ -416,9 +162,7 @@ function App() {
                                     ? 'actionButton actionError'
                                     : 'actionButton'
                             }
-                            onClick={() => {
-                                onUseAction()
-                            }}
+                            onClick={onUseActionClick}
                         >
                             Потратить действие
                         </button>
---
+++ src/hooks/useGameState.js
@@ -0,0 +1,310 @@
+import { useState, useEffect } from 'react'
+import {
+    fighters,
+    createFighter,
+    updateFighter,
+    battleUpdate,
+} from '../data/fighters'
+import { defaultPresets, createPreset } from '../data/preset'
+import { setCurrentId, currentId } from '../UTILITES/idCreator'
+import { updatePreset } from '../data/preset'
+
+const useGameState = (setConfirmModal) => {
+    // States and Effects
+
+    const [gameState, setGameState] = useState({
+        fighters: fighters.sort((a, b) => b.initiative - a.initiative),
+        presets: [...defaultPresets],
+        round: 1,
+        currentFighter: 0,
+    })
+
+    const [prevGameState, setPrevGameState] = useState([{}])
+
+    // localStorage
+
+    const saveToLocalStorage = () => {
+        localStorage.setItem('gameState', JSON.stringify(gameState))
+        localStorage.setItem('currentId', JSON.stringify(currentId))
+    }
+
+    const loadFromLocalStorage = () => {
+        try {
+            let storedGameState = localStorage.getItem('gameState')
+            if (storedGameState) {
+                storedGameState = JSON.parse(storedGameState)
+                setGameState((prev) => {
+                    return {
+                        ...storedGameState,
+                        fighters: storedGameState.fighters.map((e) => {
+                            return updateFighter(e, e)
+                        }),
+                        presets: storedGameState.presets.map((e) => {
+                            return updatePreset(e, e)
+                        }),
+                    }
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+        let storedCurrentId = localStorage.getItem('currentId')
+        if (storedCurrentId) {
+            storedCurrentId = Number(JSON.parse(storedCurrentId))
+            setCurrentId(storedCurrentId)
+        }
+    }
+
+    // Fighter
+
+    const addFighter = (data) => {
+        setPrevGameState((prev) => {
+            return [...prev, gameState]
+        })
+        setGameState((prev) => {
+            return {
+                ...prev,
+                fighters: [...prev.fighters, createFighter(data)].sort(
+                    (a, b) => b.initiative - a.initiative
+                ),
+            }
+        })
+
+        if (data.saveAsPreset) {
+            addPreset(data)
+        }
+    }
+
+    const deleteFighter = (fighter) => {
+        setPrevGameState((prev) => {
+            return [...prev, gameState]
+        })
+        setGameState((prev) => {
+            return {
+                ...prev,
+                fighters: prev.fighters
+                    .filter((e) => e.id !== fighter.id)
+                    .sort((a, b) => b.initiative - a.initiative),
+            }
+        })
+    }
+
+    const deleteAllFighters = () => {
+        setPrevGameState((prev) => {
+            return [...prev, gameState]
+        })
+        setGameState((prev) => {
+            return {
+                ...prev,
+                fighters: [],
+            }
+        })
+    }
+
+    const editFighter = (data, fighter) => {
+        setPrevGameState((prev) => {
+            return [...prev, gameState]
+        })
+        setGameState((prev) => {
+            return {
+                ...prev,
+                fighters: prev.fighters.map((e) => {
+                    if (e.id === fighter.id) {
+                        return updateFighter(data, fighter)
+                    } else return e
+                }),
+            }
+        })
+    }
+
+    // Preset
+
+    const addPreset = (data) => {
+        setGameState((prev) => {
+            return { ...prev, presets: [...prev.presets, createPreset(data)] }
+        })
+    }
+
+    const deletePreset = (preset) => {
+        setGameState((prev) => {
+            return {
+                ...prev,
+                presets: [...prev.presets].filter((e) => e.id !== preset.id),
+            }
+        })
+    }
+
+    const editPreset = (data, preset) => {
+        setGameState((prev) => {
+            return {
+                ...prev,
+                presets: prev.presets.map((e) => {
+                    if (e.id === preset.id) {
+                        return updatePreset(data, preset)
+                    } else return e
+                }),
+            }
+        })
+    }
+
+    // interface and actions
+
+    const backUp = () => {
+        if (prevGameState.length > 1) {
+            setGameState((prev) => {
+                return { ...prevGameState[prevGameState.length - 1] }
+            })
+            setPrevGameState((prev) => {
+                return [...prev].slice(0, -1)
+            })
+        }
+    }
+
+    const onEndTurn = () => {
+        if (gameState.fighters.length !== 0) {
+            setPrevGameState((prev) => {
+                return [...prev, gameState]
+            })
+            setGameState((prev) => {
+                if (prev.currentFighter + 1 === prev.fighters.length) {
+                    return {
+                        ...prev,
+                        currentFighter: 0,
+                        round: prev.round + 1,
+                        fighters: prev.fighters.map((e) => {
+                            return {
+                                ...e,
+                                reaction: true,
+                                action: e.maxActions,
+                            }
+                        }),
+                    }
+                } else {
+                    return {
+                        ...prev,
+                        currentFighter: prev.currentFighter + 1,
+                    }
+                }
+            })
+        }
+    }
+
+    const onReactionUse = (f) => {
+        setPrevGameState((prev) => {
+            return [...prev, gameState]
+        })
+        setGameState((prev) => {
+            return {
+                ...prev,
+                fighters: prev.fighters.map((e) => {
+                    if (e.id === f.id) {
+                        const newReaction = e.reaction ? false : true
+                        return { ...e, reaction: newReaction }
+                    } else return e
+                }),
+            }
+        })
+    }
+
+    const onEndCombat = () => {
+        setGameState((prev) => {
+            return {
+                ...prev,
+                currentFighter: 0,
+                round: 1,
+                fighters: prev.fighters.map((e) => {
+                    return {
+                        ...e,
+                        reaction: true,
+                        action: e.maxActions,
+                    }
+                }),
+            }
+        })
+        setConfirmModal({
+            isOpen: true,
+            onConfirm: deleteAllFighters,
+            message: 'Удалить всех бойцов?',
+        })
+    }
+
+    const onUseAction = () => {
+        if (gameState.fighters.length === 0) {
+            return false
+        }
+
+        if (gameState.fighters[gameState.currentFighter].action <= 0) {
+            return false
+        }
+
+        setPrevGameState((prev) => {
+            return [...prev, gameState]
+        })
+        setGameState((prev) => {
+            return {
+                ...prev,
+                fighters: prev.fighters.map((e) => {
+                    if (e.id === prev.fighters[prev.currentFighter].id) {
+                        return {
+                            ...e,
+                            action:
+                                prev.fighters[prev.currentFighter].action - 1,
+                        }
+                    } else return e
+                }),
+            }
+        })
+
+        return true
+    }
+
+    const onAttack = () => {
+        let currentFighter = gameState.fighters[gameState.currentFighter]
+
+        battleUpdateFighter(
+            gameState.fighters[gameState.currentFighter + 1],
+            currentFighter.attack()
+        )
+    }
+
+    const battleUpdateFighter = (fighter, damage) => {
+        setPrevGameState((prev) => {
+            return [...prev, gameState]
+        })
+        setGameState((prev) => {
+            return {
+                ...prev,
+                fighters: prev.fighters.map((e) => {
+                    if (e.id === fighter.id) {
+                        return battleUpdate(fighter, damage)
+                    } else return e
+                }),
+            }
+        })
+    }
+
+    // RETURN
+
+    return {
+        gameState,
+        backUp,
+        onEndTurn,
+        loadFromLocalStorage,
+        saveToLocalStorage,
+        addFighter,
+        editFighter,
+        deleteAllFighters,
+        deleteFighter,
+        addPreset,
+        editPreset,
+        deletePreset,
+        onEndCombat,
+        onReactionUse,
+        onUseAction,
+        battleUpdateFighter,
+        onAttack,
+    }
+}
+
+export default useGameState
